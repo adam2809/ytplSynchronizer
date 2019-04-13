061fad8ec845396a0c0df1322b0ad407ec882b8e
psyak14 <psyak14@nottingham.ac.uk>
2019-04-13 19:29:16 +0100

    Implemented a function that puts a file on a connected android device

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from os import system as run_command
+import subsystem
+
+def run_command(command):
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    # Returns a (output, error) pair
+    return process.communicate()
 
 def put_file_on_connected_device(source, dest):
-    print(run_command(f"adb push {source} {dest}"))
+    output, error = run_command(f"adb push {source} {dest}")
+    print(output)
