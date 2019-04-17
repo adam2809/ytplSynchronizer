00b3af6e7c3a5ed7e4340ed0e321e9fc460a36a0
adam2809 <adam.kule@gmail.com>
2019-04-17 16:49:25 +0200

    Implemented a function which extracts links to videos from a video playlist also minor refacrtorization

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import subprocess
 import os
 import pprint
 from pytube import YouTube
+from bs4 import BeautifulSoup
+import requests
 
-AUDIO_FILES_DEST_PATH = 'downloadedAudioFiles'
+DOWNLOADED_FILES_PATH = 'downloadedAudioFiles'
+DEST_PATH_ON_DEVICE = '/sdcard/testPlaylist'
+
+prnt = pprint.PrettyPrinter(indent=4).pprint
 
 def run_command(command):
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
@@ -13,13 +18,15 @@ def run_command(command):
 
 def put_file_on_connected_device(source, dest):
     output, error = run_command(f"adb push {source} {dest}")
+    output, error = str(output), str(error)
     if error:
-        print(f"An error ocurred:\n\n{error}\n\n")
-    print("Output of the command:\n\n")
+        print(f"An error ocurred:")
+        print(error)
+    print("Output of the command:")
     print(output)
 
 
-def downloadedAudioFilesFromYT(urls):
+def download_audio_files_from_yt(urls):
     for url in urls:
         try:
             ytVideo = YouTube(url)
@@ -28,12 +35,33 @@ def downloadedAudioFilesFromYT(urls):
         audioFiles = ytVideo.streams.filter(only_audio=True).all()
         print("Downloading video...")
         try:
-            audioFiles[1].download(AUDIO_FILES_DEST_PATH)
+            audioFiles[1].download(DOWNLOADED_FILES_PATH)
         except:
             print("Error while downloading video!")
         print("Video downloaded!")
 
 
+def get_yt_playlist_contents(playlist_url):
+    response = requests.get(playlist_url)
+    soup = BeautifulSoup(response.text,'lxml')
+    wanted_class = 'pl-video-title-link'
+    found_links = []
+    for a in soup.find_all('a'):
+        try:
+            curr_class = a['class']
+        except:
+            continue
+        if wanted_class in curr_class:
+            found_links.append(a['href'])
+    return found_links
+
+
 if __name__ == '__main__':
-    urls = ['https://www.youtube.com/watch?v=xWOoBJUqlbI']
-    downloadedAudioFilesFromYT(urls)
+    # Test for downloading audio from a video and putting in on the connected device
+    # urls = ['https://www.youtube.com/watch?v=xWOoBJUqlbI']
+    # download_audio_files_from_yt(urls)
+    # put_file_on_connected_device(DOWNLOADED_FILES_PATH, DEST_PATH_ON_DEVICE)
+
+    # Test for getting a list of video urls from a playlist
+    url = 'https://www.youtube.com/playlist?list=PL1WyaSvUwdxcXb-V08h4rsRLjGCjKoUVd'
+    print(get_yt_playlist_contents(url))
