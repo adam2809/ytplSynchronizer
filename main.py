a07113c7484ab8b5b2097f210045f53b3e906e34
adam2809 <adam.kule@gmail.com>
2019-04-22 14:05:34 +0200

    Created a stub for PlaylistSynchronizer class and renamed utils.py to ytutils.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import subprocess
 import os
 import pprint
 
+from ytutils import YouTubePlaylistInfo, YoutubeAudioFilesDownloader
+
 TRACKED_PLAYLISTS_FILE_PATH = 'trackedPlaylists.txt'
 
 prnt = pprint.PrettyPrinter(indent=4).pprint
@@ -34,9 +36,40 @@ def get_tracked_playlists():
     return trackedPlaylists
 
 
+class PlaylistSynchronizer:
+    BASE_PATH_LOCAL = 'synchedPlaylistsLocalPath'
+    BASE_PATH_ON_DEVICE = '/sdcard/'
+
+
+    def __init__(self, url):
+        self.playlist_info = YouTubePlaylistInfo(url)
+        self.curr_synched_urls = []
+        self.urls_to_delete = []
+        self.urls_to_download = playlist_info.content_urls
+
+
+    def synchronize(self):
+        pass
+
+
+    def update_which_to_delete_and_download(self):
+        pass
+
+
+    def delete(self):
+        pass
+
+
+    def download(self):
+        pass
+
+
+    def put_downloaded_files_on_device(self):
+        pass
 
 
 
+'An error ocurred while extracting the playlists info. Make sure the URL is correct. The synchronizer initialization is aborted'
 
 if __name__ == '__main__':
     test_playlist = YouTubePlaylist('https://www.google.com/')
