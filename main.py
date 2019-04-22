01fba66457a7daa7054725e3ab69bab2b8c777bb
adam2809 <adam.kule@gmail.com>
2019-04-22 13:36:13 +0200

    Moved YoutubeAudioFilesDownloader and YouTubePlaylist classes to a seperate file

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import subprocess
 import os
 import pprint
-from pytube import YouTube
-from bs4 import BeautifulSoup
-import requests
 
 TRACKED_PLAYLISTS_FILE_PATH = 'trackedPlaylists.txt'
 
@@ -37,72 +34,8 @@ def get_tracked_playlists():
     return trackedPlaylists
 
 
-def ziplist(a,b):
-    return [list(x) for x in zip(a,b)]
 
 
-
-class YoutubeAudioFilesDownloader:
-    def __init__(self, urls, dest_path):
-        # urls get zipped with an array of Falses to keep track of which files were
-        # already downloaded (False if not True if yes)
-        self.url_isdownloaded_pairs = ziplist(urls,[False for i in range(len(urls))])
-        self.dest_path = dest_path
-
-
-    def download_all_files_from_urls(self):
-        list(map(self.download_error_checks,self.url_isdownloaded_pairs))
-
-
-    def download_error_checks(self, url_isdownloaded_pair):
-        url, is_downloaded = url_isdownloaded_pair
-        if is_downloaded:
-            print(f"{url} has already been downloaded")
-            return
-        try:
-            self.download_file_from_url(url)
-        except:
-            print(f"Error while downloading {url}")
-        else:
-            url_isdownloaded_pair[1] = True
-
-
-    def download_file_from_url(self, url):
-        yt_video = YouTube(url)
-        audio_files = yt_video.streams.filter(only_audio=True).all()
-        print(f"Downloading {yt_video.title}...")
-        audio_files[1].download(self.dest_path)
-        print(f"{yt_video.title} was downloaded successfully!")
-
-
-class YouTubePlaylist:
-    def __init__(self, url):
-        self.url = url
-        response = requests.get(url)
-        self.soup = BeautifulSoup(response.text,'lxml')
-        self.update_info()
-
-
-    def update_info(self):
-        self.get_content_urls()
-        self.get_title()
-
-
-    def get_content_urls(self):
-        wanted_class = 'pl-video-title-link'
-        found_links = []
-        for a in self.soup.find_all('a'):
-            try:
-                curr_class = a['class']
-            except:
-                continue
-            if wanted_class in curr_class:
-                found_links.append(a['href'])
-        self.content_urls = found_links.copy()
-
-
-    def get_title(self):
-        self.title = self.soup.find('h1',attrs={'class':'pl-header-title'}).text.strip()
 
 
 if __name__ == '__main__':
